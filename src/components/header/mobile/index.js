099f12ae6cfaa825fbfd3134aa5039d341998c1d
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-10-09 07:52:01 -0400

    edit styling of menu and logo

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -4,32 +4,31 @@ import "./mobile.css";
 function Mobile({ isOpen, setIsOpen }) {
   return (
     <div className="mobile">
-      <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
-        <i class="fi fi-sr-cross-circle"></i>
-      </div>
-      {/* "mobile-options" is parent container */}
+          <div className="close-icon" onClick = {() => setIsOpen(!isOpen)}>
+          <i class="fi-rr-cross-circle"></i>
       <div className="mobile-options">
-        <div className="mobile-option">
-          <a href="#project">
-            <i class="fi fi-rr-edit-alt option-icon"></i>Projects
-          </a>
-        </div>
-        <div className="mobile-option">
-          <a href="#skills">
-            <i class="fi fi-rr-laptop option-icon"></i>Skills
-          </a>
-        </div>
-        <div className="mobile-option">
-          <a href="#work">
-            <i class="fi fi-rr-briefcase option-icon"></i>Work
-          </a>
-        </div>
-        <div className="mobile-option">
-          <a href="#Contact">
-            <i class="fi fi-rr-headset option-icon"></i>Contact
-          </a>
-        </div>
-      </div>
+              <div className="mobile-option">
+                  <a href="#project">
+                      <i class="fi fi-rr-edit-alt option-icon"></i>Projects
+                  </a>
+              </div>
+              <div className="mobile-option">
+                  <a href="#skills">
+                      <i class="fi fi-rr-laptop option-icon"></i>Skills
+                  </a>
+              </div>
+              <div className="mobile-option">
+                  <a href="#work">
+                      <i class="fi fi-rr-briefcase option-icon"></i>Work
+                  </a>
+              </div>
+              <div className="mobile-option">
+                  <a href="#Contact">
+                      <i class="fi fi-rr-headset option-icon"></i>Contact
+                  </a>
+              </div>
+          </div>
+          </div>
     </div>
   );
 }
